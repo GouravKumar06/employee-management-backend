8b11bd9d52a702d42fcccee2b33a9074b829cefe
GouravKumar06 <gka98963@gmail.com>
2024-01-21 12:38:05 +0530

    changes

--- index.js
+++ index.js
@@ -18,10 +18,7 @@ app.use("/",express.static("uploads"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-    origin:"http://localhost:3001",
-    credentials:true
-}));
+app.use(cors());
 
 app.use("/api/v1/admin",admin);
 app.use("/api/v1/category",category);
